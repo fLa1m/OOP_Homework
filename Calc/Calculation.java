ba806d086bd431f64e8326269b879e2de9c720ef
Andrew Mokin <parkovka.mokin@yandex.ru>
2023-07-07 17:43:31 +0300

    Homework2 to Calc

---
+++ Calc/Calculation.java
@@ -0,0 +1,72 @@
+
+public class Calculation implements Operations {
+    private double a;
+    private double b;
+    private String operation;
+    private double result;
+
+    @Override
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    @Override
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    @Override
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    @Override
+    public double division(double a, double b) {
+        return a / b;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculation [a=" + a + ", b=" + b + ", operation=" + operation + ", result=" + result + "]";
+    }
+
+    public boolean checkZero(double b, String op) {
+        if (b == 0 && op.equals("/")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ Calc/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculation calc = new Calculation();
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("1ое число: ");
+        calc.setA(inputScanner.nextDouble());
+        System.out.println("Математическая операция (+, -, *, /): ");
+        inputScanner.nextLine();
+        calc.setOperation(inputScanner.nextLine());
+        System.out.println("2ое число: ");
+        calc.setB(inputScanner.nextDouble());
+
+        switch (calc.getOperation()) {
+            case ("+"):
+                calc.setResult(calc.addition(calc.getA(), calc.getB()));
+                break;
+            case ("-"):
+                calc.setResult(calc.subtraction(calc.getA(), calc.getB()));
+                break;
+            case ("*"):
+                calc.setResult(calc.multiplication(calc.getA(), calc.getB()));
+                break;
+            case ("/"):
+                if (calc.checkZero(calc.getB(), calc.getOperation())) {
+                    calc.setResult(calc.division(calc.getA(), calc.getB()));
+                } else {
+                    System.out.println("На ноль делить нельзя");
+                }
+                break;
+        }
+        if (calc.checkZero(calc.getB(), calc.getOperation())) {
+            System.out.println(calc.getA() + " " + calc.getOperation() + " " +
+                    calc.getB() + " = " + calc.getResult());
+        }
+        inputScanner.close();
+    }
+}
